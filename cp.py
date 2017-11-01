cf31a4852c54445cf438b91e2c5455470ce86809
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-11-01 15:56:55 +0100

    first commit

---
+++ cp.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+from matplotlib.finance import candlestick_ohlc
+import numpy as np
+import urllib
+import ssl
+import datetime as dt
+import time
+
+example = time.time()
+print(dt.datetime.fromtimestamp(example))
+
+
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+
+    return bytesconverter
+
+
+def graph_data(stock):
+    # Unfortunately, Yahoo's API is no longer available
+    # feel free to adapt the code to another source, or use this drop-in replacement.
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
+
+    stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
+    source_code = urllib.request.urlopen(stock_price_url).read().decode()
+    stock_data = []
+    split_source = source_code.split('\n')
+    for line in split_source[1:]:
+        split_line = line.split(',')
+        if len(split_line) == 7:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(line)
+
+    date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data,
+                                                                      delimiter=',',
+                                                                      unpack=True,
+                                                                      converters={0: bytespdate2num('%Y-%m-%d')})
+
+    x = 0
+    y = len(date)
+    ohlc = []
+    while x < y:
+        append_me = date[x], openp[x], highp[x], lowp[x], closep[x], volume[x]
+        ohlc.append(append_me)
+        x += 1
+        candlestick_ohlc(ax1, ohlc, width=0.4, colorup='g', colordown='r')
+        for label in ax1.xaxis.get_ticklabels():
+            label.set_rotation(45)
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        ax1.set_major_locator(mticker.MaxNLocator(10))
+        ax1.grid(True)
+
+    ax1.fill_between(date, closep, closep[0], where=(closep > closep[0]), facecolor='g', alpha=0.3)
+    ax1.fill_between(date, closep, closep[0], where=(closep < closep[0]), facecolor='r', alpha=0.3)
+    # ax1.fill_between(date,closep,0, alpha = 0.3)
+
+
+graph_data('EBAY')
+# graph_data('TSLA')
